4490d1c8999cd14572a30329fa3c1a55b1162413
VjRawat <Vijay Rawat>
2018-08-14 10:52:22 +0530

    New html file added

--- restaurant/restaurantmanager/views.py
+++ restaurant/restaurantmanager/views.py
@@ -13,6 +13,10 @@ def index(request):
     return HttpResponse(template_name.render(context, request))
 
 def aboutus(request):
+    template_name = loader.get_template("About.html")
+    context = {
+        "tag1": "This text is from About.html"
+    }
 
 
-    return HttpResponse("Thanks fo visiting our website")
+    return HttpResponse(template_name.render(context, request))
